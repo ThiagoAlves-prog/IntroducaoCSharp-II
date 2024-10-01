8e7c3069baa69935fc7578914d6bfdd1b4d076b2
ThiagoAlves-prog <thiago.j.alves7@aluno.senai.br>
2024-10-01 14:15:12 -0300

    Exercises for c# with Visual Studio

---
+++ Atv-11/Atv-11/Program.cs
@@ -0,0 +1,20 @@
+﻿
+try
+{
+  Console.Write("Digite o primeiro número para realizar sua soma: ");
+    double num1 = double.Parse(Console.ReadLine());
+
+    Console.Write("Digite o segundo número: ");
+   double num2 = double.Parse(Console.ReadLine());
+
+    double resultado = num1 + num2;
+    Console.Write($"O resultado da soma é: {resultado}");
+}
+
+catch (FormatException)
+{
+   Console.WriteLine("Erro: por favor, degite numeros válidos");
+}
+
+Console.WriteLine("\nDigite qualquer tecla para encerrar o programa");
+Console.ReadKey();
---
+++ Atv-16/Atv-16/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Digite um número inteiro positivo: ");
+
+if (int.TryParse(Console.ReadLine(), out int numero) && numero >= 0)
+{
+    int fatorial = 1;
+
+
+    for (int i = 1; i <= numero; i++)
+    {
+        fatorial *= i;
+    }
+
+
+    Console.WriteLine($"O fatorial de {numero} é  = {fatorial}");
+}
+else
+{
+    Console.WriteLine("Por favor, insira um número inteiro positivo.");
+}
+
+Console.WriteLine("Digite qualquer tecla para encerrar!");
+Console.ReadKey();
